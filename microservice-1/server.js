050bb68fcc06186d1e53c07a2b9a21ae3ea40ad4
NailyaKhafaeva <webalienn@gmail.com>
2023-08-11 13:51:46 +0300

    added test task

---
+++ microservice-1/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+const amqplib = require('amqplib');
+let channel, connection;
+
+const taskQueue = 'task_queue';
+const resultQueue = 'result_queue';
+
+connect();
+
+// покдлючение к rabbitMQ
+async function connect() {
+    try {
+        const amqpServer = 'amqp://localhost:5672';
+        connection = await amqplib.connect(amqpServer);
+        channel = await connection.createChannel();
+
+        console.log('Connected to the "amqp://localhost:5672"');
+
+        await channel.assertQueue(resultQueue);
+        await channel.assertQueue(taskQueue);
+
+        await channel.consume(resultQueue, (message) => {
+            console.log('Consuming "result_queue"');
+            console.log(`Received ${Buffer.from(message.content)}`);
+            channel.ack(message);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.post('/api', (req, res) => {
+    const message = req.body;
+
+    console.log(`Received POST request with body ${JSON.stringify(message)}`);
+
+    // отправление сообщения на все сервисы, которые прослушивают очередь 'task_queue'
+    channel.sendToQueue(
+        taskQueue,
+        Buffer.from(
+            JSON.stringify({
+                ...message,
+                date: new Date(),
+            })
+        )
+    );
+
+    res.send(`Message sent`);
+    console.log(`Message ${JSON.stringify(message)} sent to "task_queue"`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
---
+++ microservice-2/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 3001;
+
+const amqplib = require('amqplib');
+let channel, connection;
+
+const taskQueue = 'task_queue';
+const resultQueue = 'result_queue';
+
+connect();
+
+// покдлючение к rabbitMQ
+async function connect() {
+    try {
+        const amqpServer = 'amqp://localhost:5672';
+        connection = await amqplib.connect(amqpServer);
+        channel = await connection.createChannel();
+
+        console.log('Connected to the "amqp://localhost:5672"');
+
+        await channel.assertQueue(resultQueue);
+        await channel.assertQueue(taskQueue);
+
+        await channel.consume(taskQueue, (message) => {
+            console.log('Consuming "task_queue"');
+            console.log(`Received ${Buffer.from(message.content)}`);
+            channel.ack(message);
+
+            let editedMessage = editMessage(message);
+
+            console.log('Received message edited');
+
+            // отправление сообщения на все сервисы, которые прослушивают очередь 'result_queue'
+            channel.sendToQueue(
+                resultQueue,
+                Buffer.from(
+                    JSON.stringify({
+                        editedMessage,
+                        date: new Date(),
+                    })
+                )
+            );
+
+            console.log('Edited message sent to "result_queue"');
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function editMessage(message) {
+    message = 'meow';
+    return message;
+}
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
